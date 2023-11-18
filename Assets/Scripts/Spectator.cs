b3377ce098e02d3d20e9773e575ff7f2d6a17187
DucTato <65166409+DucTato@users.noreply.github.com>
2023-11-18 19:57:25 +0700

    Arms Reworked
    Reworked on the arm (lowered the number of joints). Borrowed simple Inverse Kinematic algorithm.

--- Assets/Scripts/Spectator.cs
+++ Assets/Scripts/Spectator.cs
@@ -7,7 +7,7 @@ public class Spectator : MonoBehaviour
     public static Spectator instance;
     private Camera mainCam;
     //initial speed
-    public int speed = 20;
+    public int speed = 10;
     private void Awake()
     {
         instance = this;
@@ -28,38 +28,38 @@ public class Spectator : MonoBehaviour
         //press shift to move faster
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
-            speed = 40;
+            speed = 20;
 
         }
         else
         {
             //if shift is not pressed, reset to default speed
-            speed = 20;
+            speed = 10;
         }
         //For the following 'if statements' don't include 'else if', so that the user can press multiple buttons at the same time
         //move camera to the left
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = transform.position + mainCam.transform.right * -1 * speed * Time.deltaTime;
+            transform.position += mainCam.transform.right * -1 * speed * Time.deltaTime;
         }
 
         //move camera backwards
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position = transform.position + mainCam.transform.forward * -1 * speed * Time.deltaTime;
+            transform.position += mainCam.transform.forward * -1 * speed * Time.deltaTime;
 
         }
         //move camera to the right
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position = transform.position + mainCam.transform.right * speed * Time.deltaTime;
+            transform.position += mainCam.transform.right * speed * Time.deltaTime;
 
         }
         //move camera forward
         if (Input.GetKey(KeyCode.W))
         {
 
-            transform.position = transform.position + mainCam.transform.forward * speed * Time.deltaTime;
+            transform.position += mainCam.transform.forward * speed * Time.deltaTime;
         }
     }
 }
