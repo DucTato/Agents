db995a700d6670a666a76d7665dd82a814a09abc
DucTato <79449049+Minh13022001@users.noreply.github.com>
2024-01-03 23:38:38 +0700

    Refactored cube logic
    fixed some errors in the cube scripts

---
+++ Assets/Scripts/Cubes/PrioritizedCube.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class PrioritizedCube : MonoBehaviour
+{
+    private GameObject initialMattress;
+    private float touchGround;
+    private bool touchedGround;
+    private void Awake()
+    {
+        //Generate a color for the cube
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+    private void Start()
+    {
+        gameObject.SetActive(false);
+        ReturnToSpawn();
+    }
+    private void Update()
+    {
+        if (touchedGround && gameObject.CompareTag("Priority"))
+        {
+            touchGround += Time.deltaTime;
+            if (touchGround >= 15f)
+            {
+                // If a cube stay idle on the ground for 15 seconds then reset the cube's position
+                ReturnToSpawn();
+            }
+        }
+        else
+        {
+            touchGround = 0f;
+        }
+    }
+    private void ReturnToSpawn()
+    {
+        touchedGround = false;
+        touchGround = 0;
+        //Locate the initial mattress that spawned this cube
+        if (initialMattress != null)
+        {
+            BoxCollider boundary = initialMattress.GetComponent<BoxCollider>();
+            float randomX = Random.Range(boundary.bounds.center.x - 2f, boundary.bounds.center.x + 2f);
+            float randomZ = Random.Range(boundary.bounds.center.z - 1f, boundary.bounds.center.z + 1f);
+            float randomY = Random.Range(0.7f, 4f);
+            transform.SetPositionAndRotation(new Vector3(randomX, randomY, randomZ), initialMattress.transform.rotation);
+        }
+    }
+    private void OnDisable()
+    {
+        ReturnToSpawn();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            touchedGround = true;
+        }
+    }    
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            touchedGround = false; 
+            touchGround = 0;
+        }
+    }
+    public GameObject SetSpawn(GameObject mattress)
+    {
+        initialMattress = mattress;
+        return gameObject;
+    }
+}
