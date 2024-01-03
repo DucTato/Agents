6296dff1a10e93a6746568c26d8c3f6a044271a6
DucTato <67426767+cungaminoan@users.noreply.github.com>
2024-01-03 13:31:32 +0700

    UI update 2
    Completed the basic functions of the menu

--- Assets/Scripts/Camera/MouseLook.cs
+++ Assets/Scripts/Camera/MouseLook.cs
@@ -2,52 +2,116 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    private Spectator camObject;
-    private Camera mainCamera;
+    //private Camera mainCamera;
     Vector2 rotation = Vector2.zero;
     public float lookSpeed = 3f;
     private bool enable;
+    [SerializeField]
+    private KeyCode[] inputKeycodes;
+    [SerializeField]
+    private float speed;
+    private bool movementUnlocked = true;
+    /// <summary>
+    /// index       Command
+    /// 0           cursorToggle
+    /// 1           camUp
+    /// 2           camDown
+    /// 3           camFwd
+    /// 4           camBwd
+    /// 5           camLeft
+    /// 6           camRight
+    /// 7           camSpeed
+    /// </summary>
     // Start is called before the first frame update
     void Start()
     {
-        camObject = Spectator.instance;
-        mainCamera = Camera.main;
+        //mainCamera = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible= true;
         enable = true;
-        SystemController.instance.GetComponent<UIController>().SetMainCam(mainCamera);
+        SystemController.instance.GetComponent<UIController>().SetMainCam(GetComponent<Camera>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //press Middle Mouse Button (the Wheel) to toggle MouseLook ON/OFF
-        if (Input.GetMouseButtonDown(2) && !enable)
+        //press a key to toggle MouseLook ON/OFF
+        if (Input.GetKeyDown(inputKeycodes[0]) && !enable)
         {
             enable = true;
-            camObject.ToggleMovement(enable);
+            movementUnlocked = enable;
             Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible= false;
         }
-        else if (Input.GetMouseButtonDown(2) && enable)
+        else if (Input.GetKeyDown(inputKeycodes[0]) && enable)
         {
             enable = false;
-            camObject.ToggleMovement(enable);
+            movementUnlocked = enable;
             Cursor.lockState = CursorLockMode.None;
+            Cursor.visible= true;
         }
         if (enable)
         {
             //move camera upwards
-            if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+            if (Input.GetKey(inputKeycodes[1]) || Input.GetKeyDown(inputKeycodes[1]))
             {
-                transform.position +=  mainCamera.transform.up * camObject.speed * Time.deltaTime;
+                transform.position +=  transform.up * speed * Time.deltaTime;
             }
             //move camera downwards
-            if (Input.GetMouseButtonDown(1) || Input.GetMouseButton(1))
+            if (Input.GetKey(inputKeycodes[2]) || Input.GetKeyDown(inputKeycodes[2]))
             {
-                transform.position += mainCamera.transform.up * -camObject.speed * Time.deltaTime;
+                transform.position += transform.up * -speed * Time.deltaTime;
             }
             rotation.y += Input.GetAxis("Mouse X");
             rotation.x += -Input.GetAxis("Mouse Y");
-            transform.eulerAngles = (Vector2)(rotation * lookSpeed);
+            transform.eulerAngles = (rotation * lookSpeed);
         }
+        if (movementUnlocked)
+        {
+            //Hold down a key to move faster
+            if (Input.GetKey(inputKeycodes[7]))
+            {
+                speed = 20;
+            }
+            else
+            {
+                //reset to default speed after key released
+                speed = 10;
+            }
+
+            //For the following 'if statements' don't include 'else if', so that the user can press multiple buttons at the same time
+            //move camera to the left
+            if (Input.GetKey(inputKeycodes[5]))
+            {
+                transform.position += transform.right * -speed * Time.deltaTime;
+            }
+
+            //move camera backwards
+            if (Input.GetKey(inputKeycodes[4]))
+            {
+                transform.position += transform.forward * -speed * Time.deltaTime;
+
+            }
+            //move camera to the right
+            if (Input.GetKey(inputKeycodes[6]))
+            {
+                transform.position += transform.right * speed * Time.deltaTime;
+
+            }
+            //move camera forward
+            if (Input.GetKey(inputKeycodes[3]))
+            {
+
+                transform.position += transform.forward * speed * Time.deltaTime;
+            }
+        }
+    }
+    public void RemapControl(int index,KeyCode input)
+    {
+        inputKeycodes[index] = input;
+    }
+    public KeyCode GetCurrentBind(int index)
+    {
+        return inputKeycodes[index];
     }
 }
--- Assets/Scripts/SystemControl/UIController.cs
+++ Assets/Scripts/SystemControl/UIController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,10 +8,13 @@ public class UIController : MonoBehaviour
     [SerializeField]
     private List<string> mapSelections;
     [SerializeField]
+    private Button[] controlButtons;
+    [SerializeField]
     private Dropdown sceneSelectionMenu;
     [SerializeField]
-    private GameObject pausePanel;
+    private GameObject pausePanel, mapPanel;
     private Camera mainCam;
+    private int buttonIndex;
     // Start is called before the first frame update
     void Start()
     { 
@@ -20,6 +22,13 @@ public class UIController : MonoBehaviour
         mapSelections.Remove(gameObject.scene.name);
         sceneSelectionMenu.AddOptions(mapSelections);
         sceneSelectionMenu.captionText.text = gameObject.scene.name;
+
+        // Add a listener to each button
+        for (int i = 0; i <controlButtons.Length; i++)
+        {
+            int tempValue = i;
+            controlButtons[i].onClick.AddListener(() => ButtonPress(tempValue));
+        }
     }
 
     // Update is called once per frame
@@ -30,23 +39,62 @@ public class UIController : MonoBehaviour
             if (pausePanel.activeInHierarchy)
             {
                 Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
                 pausePanel.SetActive(false);
                 mainCam.GetComponent<MouseLook>().enabled = true;
-                mainCam.GetComponent<Spectator>().enabled = true;  
             }
             else
             {
                 pausePanel.SetActive(true);
                 mainCam.GetComponent<MouseLook>().enabled = false;
-                mainCam.GetComponent<Spectator>().enabled = false;
                 Cursor.lockState = CursorLockMode.None;
+                // Upon activating the pause panel, update all the buttons
+                for (int i = 0; i < controlButtons.Length; i++)
+                {
+                    UpdateButtonText(i);
+                }
             }
         }
+        if(mapPanel.activeInHierarchy)
+        {
+            // While the map button panel is being opened, user can press any key to bind value
+            foreach (KeyCode pressed in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if(Input.GetKey(pressed))
+                {
+                    // Assign new key value for this command
+                    BindValueToKey(buttonIndex, pressed);
+                    // Update the UI elements
+                    UpdateButtonText(buttonIndex);
+                    mapPanel.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private void ButtonPress(int index)
+    {
+        //Debug.Log("Button: " + index);
+        // Store the index of this button for later use
+        buttonIndex = index;
+        mapPanel.SetActive(true);
+    }
+    private void UpdateButtonText(int index)
+    {
+        controlButtons[index].GetComponentInChildren<Text>().text = mainCam.GetComponent<MouseLook>().GetCurrentBind(index).ToString();
+    }
+    private void BindValueToKey(int index, KeyCode value)
+    {
+        mainCam.GetComponent<MouseLook>().RemapControl(index, value);
+    }
+    public void SaveButton()
+    {
+        pausePanel.SetActive(false);
     }
     public void OnSceneSelected(int option)
     {
         SceneManager.LoadScene(mapSelections[option]);
-        Debug.Log("Load scene #" + option);
+        //Debug.Log("Load scene #" + option);
     }
     public void SetMainCam(Camera cam)
     {
